ba0a287453547c3e8c1fae5383bd4b0ffbe3e7dc
skywallkered <davidmail000@gmail.com>
2019-06-09 20:51:16 +0300

    bild dir

---
+++ build/precache-manifest.d02ecc1c7411476b29792f5c973c2f9f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d382e68f7aa915760a354a91c0c3d77",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2edc715d9e5676066579",
+    "url": "/static/css/main.cc54312c.chunk.css"
+  },
+  {
+    "revision": "55abbc1938fcfff27646",
+    "url": "/static/js/2.d9b295bd.chunk.js"
+  },
+  {
+    "revision": "2edc715d9e5676066579",
+    "url": "/static/js/main.a962eb77.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
